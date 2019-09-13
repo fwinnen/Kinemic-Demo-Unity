e9c1854d901d32001df26daf40bca6250a845235
Fabian Winnen <fabian@kinemic.de>
2019-09-13 19:29:57 +0200

    kinemic engine wrapper

--- Assets/Scripts/GestureController.cs
+++ Assets/Scripts/GestureController.cs
@@ -1,247 +1,53 @@
 ﻿///////////////////////////////////////////////////////////////////////////////
-//
-// RealWear Development Software, Source Code and Object Code
-// (c) RealWear, Inc. All rights reserved.
-//
-// Contact info@realwear.com for further information about the use of this
-// code.
-//
-///////////////////////////////////////////////////////////////////////////////
-
 using UnityEngine;
 using UnityEngine.UI;
-using WearHFPlugin;
-
-
+using Kinemic.Gesture;
+	
 
 /// <summary>
 /// Class for manang the pause and resume camera buttons
 /// </summary>
-public class ButtonsController : MonoBehaviour
+public class GestureController : MonoBehaviour
 {
 
+    private Engine m_engine;
 
-    /// <summary>
-    /// The on-scren pause button
-    /// </summary>
-    public Button PauseButton;
-
-    /// <summary>
-    /// The on-screen resume button
-    /// </summary>
-    public Button ResumeButton;
-
-    private WebCamTexture m_webcam = null;
-    private WearHF m_wearHf;
-
-    private Color m_colorEnabled = Color.white;
-    private Color m_colorDisabled = Color.grey;
-
-    private AndroidJavaObject m_engine;
-
-    /// <summary>
-    /// Possible states for a button
-    /// </summary>
-    private enum ButtonState
-    {
-        Enabled,
-        Disabled
-    }
-
-    class OnGestureListener : AndroidJavaProxy
-    {
-        public delegate void Callback(int gesture);
-        private Callback m_callback;
-
-        public OnGestureListener(Callback callback) : base("de.kinemic.gesture.OnGestureListener") {
-            m_callback = callback;
-        }
-
-        public void onGesture(AndroidJavaObject gesture)
-        {
-            m_callback(gesture.Call<int>("ordinal"));
-        }
-    }
-
-    enum ConnectionState
-    {
-
-        /** The Kinemic Band is in connecting state. */
-        CONNECTING,
-
-        /** The Kinemic Band is in connected state. */
-        CONNECTED,
-
-        /** The Kinemic Band is in disconnecting state. */
-        DISCONNECTING,
-
-        /** The Kinemic Band is in disconnected state. */
-        DISCONNECTED,
-
-        /** The Kinemic Band is in reconnecting state. */
-        RECONNECTING,
-    }
-
-    class OnConnectionStateListener : AndroidJavaProxy
-    {
-        public delegate void Callback(ConnectionState state, int reason);
-        private Callback m_callback;
-
-        public OnConnectionStateListener(Callback callback) : base("de.kinemic.gesture.OnConnectionStateChangeListener")
-        {
-            m_callback = callback;
-        }
-
-        public void onConnectionStateChanged(AndroidJavaObject state, AndroidJavaObject reason)
-        {
-            m_callback((ButtonsController.ConnectionState) state.Call<int>("ordinal"), reason.Call<int>("ordinal"));
-        }
-    }
-
-    class OnAirmouseEventListener : AndroidJavaProxy
-    {
-        public delegate void Callback(float x, float y, int facing);
-        private Callback m_callback;
-
-        public OnAirmouseEventListener(Callback callback) : base("de.kinemic.gesture.OnAirmouseEventListener")
-        {
-            m_callback = callback;
-        }
-
-        public void onClick()
-        {
-
-        }
-
-        public void onMove(float x, float y, AndroidJavaObject facing)
-        {
-            m_callback(x, y, facing.Call<int>("ordinal"));
-        }
-    }
+    private float m_x;
 
     /// <summary>
     /// See Unity docs
     /// </summary>
     private void Start()
     {
-        if (Input.location.isEnabledByUser)
-        {
-            Debug.Log("Location enabled");
-        } else
-        { 
-            Debug.Log("Location disabled");
-            Input.location.Start(500);
-        }
-        Debug.Log("ButtonController Start()");
-        //m_webcam =
-        //    GameObject.Find("RawImage").
-        //    GetComponent<CameraController>().
-        //    Webcam;
+        m_engine = new Engine();
 
-        //m_wearHf = GameObject.Find("WearHF Manager").GetComponent<WearHF>();
-        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-        AndroidJavaObject context = activity.Call<AndroidJavaObject>("getApplicationContext");
-
-        m_engine = new AndroidJavaObject("de.kinemic.gesture.Engine", context);
-        m_engine.Call("registerOnGestureListener", new OnGestureListener((gesture) =>
+        m_engine.RegisterOnGestureListener(new OnGestureListener((gesture) =>
         {
-            Debug.Log("ButtonController OnGesture(" + gesture +")");
+            Debug.Log("ButtonController OnGesture(" + gesture + ")");
         }));
 
-        m_engine.Call("registerOnAirmouseEventListener", new OnAirmouseEventListener((x, y, facing) =>
+        m_engine.RegisterOnAirmouseEventListener(new OnAirmouseEventListener((x, y, facing) =>
         {
+            m_x = x;
             //Debug.Log("ButtonController OnMove(" + x + ", " + y + ", " + facing + ")");
         }));
 
-        m_engine.Call("registerOnConnectionStateChangeListener", new OnConnectionStateListener((state, reason) =>
+        m_engine.RegisterOnConnectionStateChangeListener(new OnConnectionStateChangeListener((state, reason) =>
         {
             Debug.Log("ButtonController OnConnectionStateChanged(" + state + ", " + reason + ")");
 
             if (state == ConnectionState.CONNECTED)
             {
-                m_engine.Call("startAirmouse");
+                m_engine.StartAirmouse();
             }
         }));
 
 
-        m_engine.Call("connectStrongest", null);
-
-
-        ToggleButtonState(
-            PauseButton,
-            PauseButton.interactable ?
-                ButtonState.Enabled : ButtonState.Disabled);
-
-        ToggleButtonState(
-            ResumeButton,
-            ResumeButton.interactable ?
-                ButtonState.Enabled : ButtonState.Disabled);
+        m_engine.ConnectStrongest();
     }
 
-    /// <summary>
-    /// Pause the camera
-    /// </summary>
-    public void PauseCamera()
+    private void Update()
     {
-        m_webcam.Stop();
-
-        ToggleButtonState(PauseButton, ButtonState.Disabled);
-        ToggleButtonState(ResumeButton, ButtonState.Enabled);
-    }
-
-    /// <summary>
-    /// Resume the camera
-    /// </summary>
-    public void ResumeCamera()
-    {
-        m_webcam.Play();
-
-        ToggleButtonState(PauseButton, ButtonState.Enabled);
-        ToggleButtonState(ResumeButton, ButtonState.Disabled);
-    }
-
-    /// <summary>
-    /// Toggle the state of a button
-    /// </summary>
-    /// <param name="button">The button to toggle</param>
-    /// <param name="state">The new state for the button</param>
-    private void ToggleButtonState(Button button, ButtonState state)
-    {
-        Text buttonText = button.GetComponentInChildren<Text>();
-
-        switch (state)
-        {
-            case ButtonState.Enabled:
-                buttonText.color = m_colorEnabled;
-                button.interactable = true;
-
-                //m_wearHf.AddVoiceCommand(
-                //    buttonText.text, VoiceCommandCallback);
-                break;
-            case ButtonState.Disabled:
-                buttonText.color = m_colorDisabled;
-                button.interactable = false;
-
-                //m_wearHf.RemoveVoiceCommand(buttonText.text);
-                break;
-        }
-    }
-
-    /// <summary>
-    /// Called when a voice command is triggered by the user
-    /// </summary>
-    /// <param name="voiceCommand">The voice command that was triggered</param>
-    private void VoiceCommandCallback(string voiceCommand)
-    {
-        if (PauseButton.GetComponentInChildren<Text>().text == voiceCommand)
-        {
-            PauseCamera();
-        }
-        else if (
-            ResumeButton.GetComponentInChildren<Text>().text == voiceCommand)
-        {
-            ResumeCamera();
-        }
+        transform.Rotate(Vector3.up * m_x);
     }
 }
--- Assets/Scripts/Kinemic.Gesture.cs
+++ Assets/Scripts/Kinemic.Gesture.cs
@@ -1,7 +1,248 @@
-﻿using System;
-public class NewClass
+﻿///////////////////////////////////////////////////////////////////////////////
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kinemic
 {
-    public NewClass()
-    {
-    }
+
+namespace Gesture
+{
+
+public enum AirmousePalmDirection
+{
+
+	/** Users palm is pointing downwards. */
+	DOWNWARDS,
+
+	/** Users palm is pointing sideways. */
+	SIDEWAYS,
+
+	/** Users palm is pointing upwards. */
+	UPWARDS,
+
+	/** The system could not conclude the users palm orientation. */
+	INCONCLUSIVE,
+}
+
+public enum ConnectionState
+{
+
+	/** The Kinemic Band is in connecting state. */
+	CONNECTING,
+
+	/** The Kinemic Band is in connected state. */
+	CONNECTED,
+
+	/** The Kinemic Band is in disconnecting state. */
+	DISCONNECTING,
+
+	/** The Kinemic Band is in disconnected state. */
+	DISCONNECTED,
+
+	/** The Kinemic Band is in reconnecting state. */
+	RECONNECTING,
+}
+
+public enum ConnectionReason
+{
+	NONE,
+	PENDING,
+	SUCCESS,
+	CONNECTION_FAILED,
+	BOARD_SETUP_FAILED,
+	LOW_BATTERY,
+	INCOMPATIBLE_DEVICE,
+}
+
+public enum ActivationState
+{
+
+	/**
+	 * The button was pressed to temporarily disable gesture recognition.
+	 */
+	INACTIVE,
+
+	/**
+	 * The Kinemic Band is active, gestures are recognized.
+	 */
+	ACTIVE,
+}
+
+public enum Gesture
+{
+	/**
+	 * The Rotate RL gesture.
+	 */
+	ROTATE_RL,
+
+	/**
+	 * The Rotate LR gesture.
+	 */
+	ROTATE_LR,
+
+	/** The Circle R gesture. */
+	CIRCLE_R,
+
+	/** The Circle L gesture. */
+	CIRCLE_L,
+
+	/** The Swipe R gesture. */
+	SWIPE_R,
+
+	/** The Swipe L gesture. */
+	SWIPE_L,
+
+	/** The Swipe Up gesture. */
+	SWIPE_UP,
+
+	/** The Swipe Down gesture. */
+	SWIPE_DOWN,
+
+	/** The Check Mark gesture. */
+	CHECK_MARK,
+
+	/** The X Mark gesture. */
+	CROSS_MARK,
+
+	/** The Eartouch R gesture. */
+	EARTOUCH_R,
+
+	/** The Eartouch L gesture. */
+	EARTOUCH_L,
+}
+
+
+public class OnGestureListener : AndroidJavaProxy
+{
+	public delegate void Callback(Gesture gesture);
+	private Callback m_callback;
+
+	public OnGestureListener(Callback callback) : base("de.kinemic.gesture.OnGestureListener")
+	{
+		m_callback = callback;
+	}
+
+	public void onGesture(AndroidJavaObject gesture)
+	{
+		m_callback((Gesture)gesture.Call<int>("ordinal"));
+	}
+}
+
+public class OnConnectionStateChangeListener : AndroidJavaProxy
+{
+	public delegate void Callback(ConnectionState state, ConnectionReason reason);
+	private Callback m_callback;
+
+	public OnConnectionStateChangeListener(Callback callback) : base("de.kinemic.gesture.OnConnectionStateChangeListener")
+	{
+		m_callback = callback;
+	}
+
+	public void onConnectionStateChanged(AndroidJavaObject state, AndroidJavaObject reason)
+	{
+		m_callback((ConnectionState)state.Call<int>("ordinal"), (ConnectionReason)reason.Call<int>("ordinal"));
+	}
+}
+
+public class OnAirmouseEventListener : AndroidJavaProxy
+{
+	public delegate void Callback(float x, float y, AirmousePalmDirection facing);
+	private Callback m_callback;
+
+	public OnAirmouseEventListener(Callback callback) : base("de.kinemic.gesture.OnAirmouseEventListener")
+	{
+		m_callback = callback;
+	}
+
+	public void onClick()
+	{
+
+	}
+
+	public void onMove(float x, float y, AndroidJavaObject facing)
+	{
+		m_callback(x, y, (AirmousePalmDirection)facing.Call<int>("ordinal"));
+	}
+}
+
+
+public class Engine
+{
+	private AndroidJavaObject m_obj;
+
+	public Engine()
+	{
+		AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		AndroidJavaObject context = activity.Call<AndroidJavaObject>("getApplicationContext");
+
+		m_obj = new AndroidJavaObject("de.kinemic.gesture.Engine", context);
+
+		// ask for permissions
+		Input.location.Start(1000);
+	}
+
+	public void RegisterOnGestureListener(OnGestureListener listener)
+	{
+		m_obj.Call("registerOnGestureListener", listener);
+	}
+
+	public void UnegisterOnGestureListener(OnGestureListener listener)
+	{
+		m_obj.Call("unregisterOnGestureListener", listener);
+	}
+
+	public void RegisterOnAirmouseEventListener(OnAirmouseEventListener listener)
+	{
+		m_obj.Call("registerOnAirmouseEventListener", listener);
+	}
+
+	public void UnregisterOnAirmouseEventListener(OnAirmouseEventListener listener)
+	{
+		m_obj.Call("unregisterOnAirmouseEventListener", listener);
+	}
+
+	public void RegisterOnConnectionStateChangeListener(OnConnectionStateChangeListener listener)
+	{
+		m_obj.Call("registerOnConnectionStateChangeListener", listener);
+	}
+
+	public void UnregisterOnConnectionStateChangeListener(OnConnectionStateChangeListener listener)
+	{
+		m_obj.Call("unregisterOnConnectionStateChangeListener", listener);
+	}
+
+	public void Connect(string band)
+	{
+		m_obj.Call("connect", band);
+	}
+
+	public void Disconnect(string band)
+	{
+		m_obj.Call("disconnect", band);
+	}
+
+	public void Vibrate(int millis)
+	{
+		m_obj.Call("vibrate", millis);
+	}
+
+	public void StartAirmouse()
+	{
+		m_obj.Call("startAirmouse");
+	}
+
+	public void StopAirmouse()
+	{
+		m_obj.Call("stopAirmouse");
+	}
+
+	public void ConnectStrongest()
+	{
+		m_obj.Call("connectStrongest", null);
+	}
+}
+
+
+	}
 }
